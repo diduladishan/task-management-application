2b50ea1faded96ea63ab19747d6e5eac5da62750
diduladishan <diduladishan14@gmail.com>
2024-11-20 01:23:45 +0530

    feat(ui): integrate ShadCN date picker component

--- app/insights/page.tsx
+++ app/insights/page.tsx
@@ -1,8 +1,12 @@
+import { DatePicker } from "./components/DatePicker";
+
 export default function InsightsPage() {
   return (
     <div>
       <h1>Tasks</h1>
       <p>Here you can manage all your insights.</p>
+
+      <DatePicker />
     </div>
   );
 }
